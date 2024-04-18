584c2e69e06cc86c270227aaaa05a41db082e901
john <bblodfon@gmail.com>
2024-04-18 09:47:47 +0200

    use 80% quantile t_max

--- generate.R
+++ generate.R
@@ -120,11 +120,13 @@ with_progress({
         aft$train(task, row_ids = part$train)
 
         # Integrated Survival Brier Score (improper) and re-weighted version (proper)
-        t_max = 364
+        # Use 80% quantile of event times in the train set as time horizon
+        event_times = task$unique_event_times(rows = part$train)
+        t_max = as.integer(quantile(event_times, probs = 0.8))
         graf_improper = msr("surv.graf", proper = FALSE, id = "graf.improper",
-                            t_max = t_max)
+                             t_max = t_max)
         graf_proper   = msr("surv.graf", proper = TRUE,  id = "graf.proper",
-                            t_max = t_max)
+                             t_max = t_max)
 
         # evaluate graf proper and improper on the test set
         # using various models, but check if training succeeded first
